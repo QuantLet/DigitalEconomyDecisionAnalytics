1c075db10b562e885d0e380337f2f11439cb4c43
Raul Cristian Bag <raulbag1996@gmail.com>
2022-11-03 10:32:31 +0100

    fix us treasury xml link

--- DEDA_WebScrapingIntro/WebScrapingIntroUnit3.py
+++ DEDA_WebScrapingIntro/WebScrapingIntroUnit3.py
@@ -13,11 +13,10 @@ We use json module to unpack data from webpage.
 """
 import requests
 import xml.dom.minidom
-response = requests.get("https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Datasets/daily_treas_bill_rates.xml")
+response = requests.get("https://home.treasury.gov/sites/default/files/interest-rates/daily_treas_bill_rates.xml")
 content = response.content
 dataDOM = xml.dom.minidom.parseString(content)
 response = requests.get("https://news.google.com/news/rss/headlines/section/q/finance%20news/finance%20news?ned=us&hl=en")
-
 
 """
 Reading JSON Data Online
